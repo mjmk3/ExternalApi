3ed21d731cbcf5fa19b0acdb5f52e9fe3acc5b53
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-02-27 00:52:41 +0300

    Spring boot external api

---
+++ src/test/java/app/exapi/externalapi/ExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.exapi.externalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
